f5d8419bdbce8347b53ba058701d5f70d46fe4fa
kartikchoubisa <f20190896@goa.bits-pilani.ac.in>
2022-09-23 04:11:51 +0530

    proposal update

--- src/pages/Proposal.jsx
+++ src/pages/Proposal.jsx
@@ -10,21 +10,48 @@ import contractAddressData from "../constants/contractAddress.json"
 
 function Proposal({ url }) {
     const [content, setContent] = useState("")
+    const [proposalDetailsOnContract, setProposalDetailsOnContract] = useState([])
+    const { Moralis, enableWeb3, isWeb3Enabled } = useMoralis()
+
     const contractAddress = contractAddressData.contractAddress
     const { runContractFunction: proposalDetail } = useWeb3Contract({
         abi: abi,
         contractAddress: contractAddress,
         functionName: "proposalDetail",
-        params: {_uri: "testing..."},
+        // update to use uri from component
+        params: {_uri: "xyz"},
     })
-    async function testFetch() {
-        const result = await proposalDetail()
-        console.log(`testFetch result: ${result}`)
+
+    async function updateProposalDetails() {
+        try{
+            const result = await proposalDetail()
+            console.log(`testFetch result`, result)
+            setProposalDetailsOnContract(result)
+
+        } catch (error) {
+            console.log(`testFetch error`, error)
+            console.log("check if web3 is enabled")
+        }
     }
+
     useEffect(() => {
-        testFetch()
+        console.log("web3 enabled? ", isWeb3Enabled)
+
+        // get proposal info from contract
+        if (isWeb3Enabled) {
+            updateProposalDetails()
+        }
+        else if (typeof window !== "undefined") {
+            if (window.localStorage.getItem("connected")) {
+                enableWeb3()
+                console.log("enabled web3")
+            }
+        }
+
         // TODO:  get proposal content using url from IPFS
-    }, [])
+        
+
+    }, [isWeb3Enabled])
 
     return (
         <div className="proposalContainer">
