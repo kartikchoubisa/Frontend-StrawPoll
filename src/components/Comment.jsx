2fa6d01dc39a28e05e65e8b1f67be668c622949e
kartikchoubisa <f20190896@goa.bits-pilani.ac.in>
2022-09-24 06:30:29 +0530

    proposal ui

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,24 +1,45 @@
 import React from "react"
 import "./Comment.css"
 
-function Comment({ content, author, likes, dislikes, replies }) {
+function Comment({ content, author, likes, dislikes, replies, newComment }) {
     return (
         <div className="commentContainer">
-            <div className="commentLEFT">
-                <div className="profilePicture">pfp</div>
-            </div>
-            <div className="commentRIGHT">
-                <div className="commentHeader">
-                    <div className="commentAuthor">{author}</div>
+            {newComment ? (
+                <div className="commentContainer">
+                    <div className="commentLEFT">
+                        <div className="profilePicture">pfp</div>
+                    </div>
+                    <div className="commentRIGHT">
+                        <div className="commentHeader">
+                            <div className="commentAuthor">self.address</div>
+                        </div>
+                        <div className="commentContent">INSERT POST</div>
+                        <div className="commentFooter">
+                            <div className="commentPost">POST BUTTON</div>
+                        </div>
+                    </div>
                 </div>
-                <div className="commentContent">{content}</div>
-                <div className="commentFooter">
-                    <div className="commentLikes">{likes}</div>
-                    <div className="commentDislikes">{dislikes}</div>
-                    <div className="commentReplies">{replies.length} replies</div>
-                    <div className="commentReply">reply</div>
+            ) : (
+                <div className="commentContainer">
+                    <div className="commentLEFT">
+                        <div className="profilePicture">pfp</div>
+                    </div>
+                    <div className="commentRIGHT">
+                        <div className="commentHeader">
+                            <div className="commentAuthor">{author}</div>
+                        </div>
+                        <div className="commentContent">{content}</div>
+                        <div className="commentFooter">
+                            <div className="commentLikes">{likes}</div>
+                            <div className="commentDislikes">{dislikes}</div>
+                            <div className="commentReplies">
+                                {replies.length} replies
+                            </div>
+                            <div className="commentReply">reply</div>
+                        </div>
+                    </div>
                 </div>
-            </div>
+            )}
         </div>
     )
 }
--- src/components/Discussion.jsx
+++ src/components/Discussion.jsx
@@ -3,21 +3,45 @@ import { useEffect, useState } from "react"
 import Comment from "./Comment.jsx"
 
 function Discussion({ url }) {
-    const [comments, setComments] = useState([{content:"good good", author: "0x123", likes: "4", dislikes: "2", replies: [{content:"no, bad", author: "0x123", likes: "5", dislikes: "3", replies: []}]}])
+    const [comments, setComments] = useState([
+        {
+            content: "good good",
+            author: "0x123",
+            likes: "4",
+            dislikes: "2",
+            replies: [
+                {
+                    content: "no, bad",
+                    author: "0x123",
+                    likes: "5",
+                    dislikes: "3",
+                    replies: [],
+                },
+            ],
+        },
+    ])
 
     useEffect(() => {
         //TODO:  get comments from backend server using proposal url on load
         console.log(comments)
-
     }, [])
 
     return (
         <div className="discussionContainer">
             {/* TODO: pass whatever  */}
-            { comments.map(({author, content, likes, dislikes, replies}) => (
-                <Comment content={content} author={author} likes={likes} dislikes={dislikes} replies={replies}/>
-            ))
-        }
+            <Comment newComment={true} />
+
+            {comments.map(({ author, content, likes, dislikes, replies }) => (
+                <Comment
+                    content={content}
+                    author={author}
+                    likes={likes}
+                    dislikes={dislikes}
+                    replies={replies}
+                    newComment={false}
+                />
+            ))}
+
         </div>
     )
 }
