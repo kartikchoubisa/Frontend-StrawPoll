3ec7072b6ad0d2eff0558b638619798e05d99412
kartikchoubisa <f20190896@goa.bits-pilani.ac.in>
2022-09-24 12:06:33 +0530

    comment updates

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,10 +1,72 @@
 import React from "react"
-import "./Comment.css"
+import { useEffect, useState } from "react"
 
-function Comment({ content, author, likes, dislikes, replies, newComment }) {
+import "./Comment.css"
+import dataConst from "../constants/data.json"
+import axios from "axios"
+
+function Comment({
+    discussion_url,
+    content,
+    author,
+    likes,
+    dislikes,
+    replies,
+    newComment,
+    getComments,
+}) {
+    const pythonApiEndpoint = dataConst.pythonApiEndpoint
+
+
+    const [triggeredPost, setTriggeredPost] = useState(false)
+
+    const [commentLikes, setCommentLikes] = React.useState(likes)
+    const [commentDislikes, setCommentDislikes] = React.useState(dislikes)
+    const [commentReplies, setCommentReplies] = React.useState(replies)
+
+    // form to add new comment
+    const [newCommentContent, setNewCommentContent] = React.useState("")
+
+    useEffect(() => {
+        //post comment to backend
+        console.log("getcomment in usefefct", typeof(getComments))
+        if (triggeredPost) {
+            getComments()
+        }
+        else {
+            console.log("not triggered")
+        }
+
+    }, [triggeredPost])
+
+    async function handlePostComment(e) {
+        const hi = "www.proposal1.com"
+        e.preventDefault()
+        try {
+            let newCommentApiEndpoint = `${pythonApiEndpoint}/discussions/${hi}`
+            console.log(`trying to post comment to discussion : ${newCommentApiEndpoint}`)
+            const response = await axios.post(newCommentApiEndpoint, {
+                content: newCommentContent,
+                author_address: "0x1234567890", //TODO: get author address from MM
+            })
+            console.log("posted comment", response.data)
+
+            // update Discussion's state
+            // console.log("getcomments is?" , typeof(fun))
+            setTriggeredPost(true)
+            await getComments()
+            setTriggeredPost(false)
+            
+        
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
     return (
         <div className="commentContainer">
             {newComment ? (
+                <form onSubmit={handlePostComment}>
                 <div className="commentContainer">
                     <div className="commentLEFT">
                         <div className="profilePicture">pfp</div>
@@ -13,12 +75,17 @@ function Comment({ content, author, likes, dislikes, replies, newComment }) {
                         <div className="commentHeader">
                             <div className="commentAuthor">self.address</div>
                         </div>
-                        <div className="commentContent">INSERT POST</div>
+                        <div className="commentContent">
+                            <input type="text" placeholder="Tell other's what you think" onChange={(e) => setNewCommentContent(e.target.value)} />
+                        </div>
                         <div className="commentFooter">
-                            <div className="commentPost">POST BUTTON</div>
+                            <div className="commentPost">
+                                <button type="submit">Post</button>
+                            </div>
                         </div>
                     </div>
                 </div>
+                </form>
             ) : (
                 <div className="commentContainer">
                     <div className="commentLEFT">
--- src/components/Discussion.jsx
+++ src/components/Discussion.jsx
@@ -1,28 +1,38 @@
 import React from "react"
 import { useEffect, useState } from "react"
 import Comment from "./Comment.jsx"
+import axios from "axios"
+import dataConst from "../constants/data.json"
+
 
 function Discussion({ url }) {
-    const [comments, setComments] = useState([
-        {
-            content: "good good",
-            author: "0x123",
-            likes: "4",
-            dislikes: "2",
-            replies: [
-                {
-                    content: "no, bad",
-                    author: "0x123",
-                    likes: "5",
-                    dislikes: "3",
-                    replies: [],
-                },
-            ],
-        },
-    ])
+    const [comments, setComments] = useState([])
+    const pythonApiEndpoint = dataConst.pythonApiEndpoint
+
+    async function getComments() {
+        
+        const discussionEndpoint = `${pythonApiEndpoint}/discussions/www.proposal1.com`
+        console.log("getting comments", discussionEndpoint)
+        
+        const response = await axios.get(discussionEndpoint)
+        console.log("got comments", response.data)
+        let comments = response.data.discussion.comments
+
+        comments = comments.map(comment => {
+            comment.author = comment.author_address // renaming
+            // adding mock likes, dislikes, and replies to comments data
+            comment.likes = 0
+            comment.dislikes = 0
+            comment.replies = []
+            return comment
+        })
+
+        setComments(comments)
+    }
 
     useEffect(() => {
         //TODO:  get comments from backend server using proposal url on load
+        getComments()
         console.log(comments)
     }, [])
 
@@ -33,12 +43,15 @@ function Discussion({ url }) {
 
             {comments.map(({ author, content, likes, dislikes, replies }) => (
                 <Comment
+                //TODO: get this from url
+                    discussion_url="www.proposal1.com" 
                     content={content}
                     author={author}
                     likes={likes}
                     dislikes={dislikes}
                     replies={replies}
                     newComment={false}
+                    getComments={getComments} //to change Discussion's state from Comment component
                 />
             ))}
 
